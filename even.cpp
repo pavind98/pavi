023294f02d8a5188694b278d5a9141c8848fcd7c
pavind98 <30829431+pavind98@users.noreply.github.com>
2017-09-05 16:27:25 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a;
+ int g;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>a;
+cout<<"enter num program by programology site"<<endl;
+cin>>g;
+while(a<g){
+ 
+ if(a%2==0)
+ {
+ cout<<"even no. ="<<a<<endl;
+ 
+ }
+ a=a+1;
+}
+}
