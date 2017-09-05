41b4a9687e4803094a0f0c180dab7002b60c300a
pavind98 <30829431+pavind98@users.noreply.github.com>
2017-09-05 16:25:11 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int a;
+ int g;
+ cout<<" enter number "<<endl;
+ cin>>a;
+ cout<<" enter number "<<endl;
+ cin>>g;
+ cout<<"*********************************"<<endl;
+ cout<<" programology  site "<<endl;
+ cout<<"**********************************"<<endl;
+ for(a<=g;)
+ {
+ cout<<a<<end;
+ a=a+2;
+ }
+}
