71a84bb45022bb9ae8ad935b97e7d3d76a694fd4
pavind98 <30829431+pavind98@users.noreply.github.com>
2017-09-06 16:03:49 +0530

    Create pr.py

---
+++ pr.py
@@ -0,0 +1,20 @@
+def main():
+n = input("Please enter a number:")
+is_prime(n)
+
+def is_prime(a):
+    x = True 
+    for i in (2, a):
+            while x:
+               if a%i == 0:
+                   x = False
+               else:
+                   x = True
+
+
+    if x:
+        print "prime"
+    else:
+        print "not prime"
+
+main()
