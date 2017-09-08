148edf3d18ba6116fc0538c175cf5a5f1a8bd6ef
pavind98 <30829431+pavind98@users.noreply.github.com>
2017-09-08 16:41:20 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int sum(int arr[], int n)
+{
+    int sum = 0; // initialize sum
+    int i;
+    for (i = 0; i < n; i++)
+    sum += arr[i];
+ 
+    return sum;
+}
+ 
+int main()
+{
+    int arr[] = {12,3,4,15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Sum of given array is %d", sum(arr, n));
+    return 0;
+}
