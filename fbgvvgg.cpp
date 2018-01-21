a89b14ba0cb6c1a59be195b7d2032bfe6e39f567
pavind98 <30829431+pavind98@users.noreply.github.com>
2018-01-21 16:35:31 +0530

    Create fbgvvgg.cpp

---
+++ fbgvvgg.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+
+int count(string s, char c)
+{
+    // Count variable
+    int res = 0;
+ 
+    for (int i=0;i<s.length();i++)
+ 
+       
+        if (s[i] == c)
+            res++;
+ 
+    return res;
+}
+int main()
+{
+    string str= "geeksforgeeks";
+    char c = 'e';
+    cout << count(str, c) << endl;
+    return 0;
+}
