8a3f11bacb3c66225f3aa22aa3f11a34a99484c6
pavind98 <30829431+pavind98@users.noreply.github.com>
2018-01-21 16:39:37 +0530

    Create 2er.c

---
+++ 2er.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int a, b, sum;
+	cout<<"Enter two number :";
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"Sum of the two number is "<<sum;
+	getch();
+}
