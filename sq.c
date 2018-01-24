7de68fd19f909759beb6fa5bacc95d9725d8b906
pavind98 <30829431+pavind98@users.noreply.github.com>
2018-01-24 16:10:03 +0530

    Create sq.c

---
+++ sq.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,Product;
+clrscr();
+printf("Enter Two Numbers\n");
+scanf("\n%d\n%d",&a,&b);
+Product=a*b;
+printf("\nProduct is =%d",Product);
+getch();
+return 0;
+}
