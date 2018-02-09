146c8ee73dd4f71c659d2aa5811e7ece73a8906d
pavind98 <30829431+pavind98@users.noreply.github.com>
2018-02-09 14:29:47 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+int main()
+{
+    char[10];
+    int i,j,b,)
+    scanf("%d",a);
+    
+    for(i=0;i<b;i++)
+    {
+        for(j=i;j<b;j++)
+        {   if(a[i]>a[j])
+            {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<b;i++)
+    {
+        printf("%c",a[i]);
+    }
+}
+
+
+
+
+    return 0;
+}
+
+
+
